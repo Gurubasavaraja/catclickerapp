8543d47945e357f053372d734e0522ae3d41c746
Guru081 <gurubasavaraj1996@gmail.com>
2019-10-18 14:58:51 +0530

    Updates

---
+++ precache-manifest.b17ba06445d2cc0118b6e34fb33d6978.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b64c6a9fc471ef9cabf5b239052fe4",
+    "url": "/catclickerapp/index.html"
+  },
+  {
+    "revision": "32dc2d151d2e5c781dae",
+    "url": "/catclickerapp/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fda486d660edb4e2dee6",
+    "url": "/catclickerapp/static/css/main.cfde8293.chunk.css"
+  },
+  {
+    "revision": "32dc2d151d2e5c781dae",
+    "url": "/catclickerapp/static/js/2.d4a7da4d.chunk.js"
+  },
+  {
+    "revision": "fda486d660edb4e2dee6",
+    "url": "/catclickerapp/static/js/main.8e9f7d98.chunk.js"
+  },
+  {
+    "revision": "9698dee30b241d632f3f",
+    "url": "/catclickerapp/static/js/runtime-main.699c6c61.js"
+  },
+  {
+    "revision": "423c5bfd182fd20a404109494421501d",
+    "url": "/catclickerapp/static/media/cat.423c5bfd.jpg"
+  }
+]);
